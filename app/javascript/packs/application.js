ae3eca947bd0b7a89783dfe79e5935689ade5c3c
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-10-23 01:27:31 +0200

    content created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,4 +38,25 @@ document.addEventListener('turbolinks:load', () => {
   bannerAnimation();
   mouseAnimation();
   navBarAnimation();
+
+  const faders = document.querySelectorAll('.fade-in');
+  const appearOptions = {
+    threshold: 0.4,
+    rootMargin: "0px 0px -100px 0px"
+  }
+
+  const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobesrve(entry.target);
+      }
+    })
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  })
 });
